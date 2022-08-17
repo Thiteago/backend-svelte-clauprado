2c4c35496feea7291f54aeaee034238da670cf31
Thiago David <thiagodavidpd@gmail.com>
2022-08-17 19:13:43 -0300

    fix post

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-require('./Routes/index')(app);
-
-const corsOptions ={
-    origin: true, 
-    methods: ['GET', 'PUT', 'POST'],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+const corsOptions = {
+  Origin: '*',
+  methods: ['GET', 'PUT', 'POST'],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(express.json());
+app.use(cors(corsOptions));
 app.listen(3333);
+
+require('./Routes/index')(app);
