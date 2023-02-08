97aa6d9b0e687b85c2242e2989ce40154c843600
Thiago <thiagodavidpd@gmail.com>
2023-02-08 16:16:53 -0300

    alterado metodo de calculo de frete

--- src/Controller/CarrinhoController.ts
+++ src/Controller/CarrinhoController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
-let Correios = require('node-correios');
-let correios = new Correios();
+
+
 
 
 
@@ -9,25 +9,13 @@ let correios = new Correios();
 export class CarrinhoController {
   async calcularFrete(req: Request, res: Response) {
     let cep = req.params.cep;
-    console.log(cep)
-    let args = {
-      nCdServico: '04014',
-      sCepOrigem: '12460000',
-      sCepDestino: cep,
-      nVlPeso: '1',
-      nCdFormato: '1',
-      nVlComprimento: '16',
-      nVlAltura: '2',
-      nVlLargura: '11',
-      nVlDiametro: '0',
-    }
 
-    correios.calcPreco(args)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.log(error);
+    const response = await fetch(`https://www.cepcerto.com/ws/json-frete/12460000/${cep}/500`, {
+      method: 'POST',
     });
+
+    const data = await response.json();
+    
+    return res.json(data);
   }
 }
