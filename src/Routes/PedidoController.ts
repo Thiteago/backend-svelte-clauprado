08fcf230616168dfd4afacc250f1fa86e4ee0e5d
thiteago <ti+thiago@seguroviagem.srv.br>
2023-02-27 22:23:20 -0300

    change pagamento controller to pedido controller

---
+++ src/Routes/PedidoController.ts
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import { PedidoController } from "../Controller/PedidoController";
+
+
+
+const pedidoController = new PedidoController()
+export const routerPedido = Router();
+
+
+routerPedido.post("/pedido/gerar", pedidoController.gerar);
+routerPedido.get("/pedido/listar/:id", pedidoController.listarPeloId);
+
+
+
+
+
+
+
