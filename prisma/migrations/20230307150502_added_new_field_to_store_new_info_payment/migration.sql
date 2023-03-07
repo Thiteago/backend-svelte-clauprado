8be714f53609b2f9954b64edf2f8aeafdff00056
Thiago <thiagodavidpd@gmail.com>
2023-03-07 12:50:35 -0300

    added payment installments field on pagaamento table

---
+++ prisma/migrations/20230307150502_added_new_field_to_store_new_info_payment/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Pagamento" ADD COLUMN     "vezes" TEXT NOT NULL DEFAULT 'A vista';
---
+++ prisma/migrations/20230307151041_remove_status_column_from_cartao/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Cartao` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Cartao" DROP COLUMN "status";
