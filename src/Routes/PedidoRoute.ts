ade456eb00fbb98ca437fccd711a734350cc4c1a
Thiago <thiagodavidpd@gmail.com>
2023-03-15 14:47:41 -0300

    create new route to list  orders by id

--- src/Routes/PedidoRoute.ts
+++ src/Routes/PedidoRoute.ts
@@ -8,7 +8,8 @@ export const routerPedido = Router();
 
 
 routerPedido.post("/pedido/gerar", pedidoController.gerar);
+routerPedido.get("/pedido/listar/user/:id", pedidoController.listarPeloUserId);
 routerPedido.get("/pedido/listar/:id", pedidoController.listarPeloId);
 routerPedido.get("/pedido/listar/", pedidoController.listarTodos);
-routerPedido.patch("/pedido/alterar/produtos/:id", pedidoController.alterarProdutos);
+routerPedido.patch("/pedido/alterar/produtos/", pedidoController.alterarProdutos);
 routerPedido.patch("/pedido/alterar/endereco/:id", pedidoController.alterarEndereco);
