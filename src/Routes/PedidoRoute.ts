3a92af9565e44e2c1332d30310cb209cbbb412a5
Thiago <thiagodavidpd@gmail.com>
2023-03-16 15:37:50 -0300

    create route to cancel an  order

--- src/Routes/PedidoRoute.ts
+++ src/Routes/PedidoRoute.ts
@@ -11,5 +11,6 @@ routerPedido.post("/pedido/gerar", pedidoController.gerar);
 routerPedido.get("/pedido/listar/user/:id", pedidoController.listarPeloUserId);
 routerPedido.get("/pedido/listar/:id", pedidoController.listarPeloId);
 routerPedido.get("/pedido/listar/", pedidoController.listarTodos);
+routerPedido.delete("/pedido/cancelar/:id", pedidoController.cancelarPedido);
 routerPedido.patch("/pedido/alterar/produtos/", pedidoController.alterarProdutos);
 routerPedido.patch("/pedido/alterar/endereco/:id", pedidoController.alterarEndereco);
