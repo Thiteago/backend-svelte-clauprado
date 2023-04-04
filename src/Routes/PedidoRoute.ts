673d49ca10373a06f5ea7cff42affb8f12d16861
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-03 22:00:19 -0300

    create new route to update info about delivery

--- src/Routes/PedidoRoute.ts
+++ src/Routes/PedidoRoute.ts
@@ -7,12 +7,17 @@ const pedidoController = new PedidoController()
 export const routerPedido = Router();
 
 
+routerPedido.delete("/pedido/cancelar/:id", pedidoController.cancelarPedido);
+
+routerPedido.post("/pedido/capturar/", pedidoController.capturarPagamento);
 routerPedido.post("/pedido/gerar", pedidoController.gerar);
+
 routerPedido.get("/pedido/listar/user/:id", pedidoController.listarPeloUserId);
 routerPedido.get("/pedido/listar/:id", pedidoController.listarPeloId);
 routerPedido.get("/pedido/listar/", pedidoController.listarTodos);
-routerPedido.delete("/pedido/cancelar/:id", pedidoController.cancelarPedido);
+
 routerPedido.patch("/pedido/alterar/produtos/", pedidoController.alterarProdutos);
 routerPedido.patch("/pedido/alterar/endereco/:id", pedidoController.alterarEndereco);
-routerPedido.patch("/pedido/informarenvio/:id", pedidoController.marcarComoEnviado);
-routerPedido.post("/pedido/capturar/", pedidoController.capturarPagamento);
+routerPedido.patch("/pedido/alterar/envio/:id", pedidoController.atualizarEnvio);
+
+
