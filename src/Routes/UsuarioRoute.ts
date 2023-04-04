e4438f1eded096248f5077b82a902e584a62e8fc
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-03 22:00:02 -0300

    create new rote to update user role

--- src/Routes/UsuarioRoute.ts
+++ src/Routes/UsuarioRoute.ts
@@ -1,7 +1,6 @@
 import {Router} from "express"
 import { UsuarioController } from "../Controller/UsuarioController";
 import { AuthController } from "../Controller/AuthController";
-import { AuthMiddleware } from "../middlewares/auth";
 
 const usuariocontroller = new UsuarioController()
 const authcontroller = new AuthController()
@@ -9,13 +8,16 @@ export const routerUser = Router();
 
 
 routerUser.post("/NovoUsuario", usuariocontroller.cadastrar);
-routerUser.get('/Usuarios', AuthMiddleware ,usuariocontroller.listar);
 routerUser.post("/Autenticar", authcontroller.authenticate)
-routerUser.get('/Usuarios/:id/dados', usuariocontroller.mostrarInfo)
-routerUser.patch('/Usuarios/:id/Alterar', usuariocontroller.alterarUser)
-routerUser.get('/Usuarios/:id/Enderecos', usuariocontroller.listarEnderecos)
 routerUser.post('/Usuarios/:id/NovoEndereco', usuariocontroller.cadastrarNovoEndereco)
+
+routerUser.get('/Usuarios', usuariocontroller.listar);
+routerUser.get('/Usuarios/:id/dados', usuariocontroller.mostrarInfo)
+routerUser.get('/Usuarios/:id/Enderecos', usuariocontroller.listarEnderecos)
+
+routerUser.patch('/Usuarios/:id/Alterar', usuariocontroller.alterarUser)
 routerUser.patch('/Usuarios/:id/AtualizarEndereco', usuariocontroller.atualizarEndereco)
+routerUser.patch("/Usuarios/alterar/cargo/:id", usuariocontroller.atualizarCargo);
 
 
 
