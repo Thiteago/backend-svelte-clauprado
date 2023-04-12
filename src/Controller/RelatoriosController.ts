d4872272b17f71252022de9effccf28f5b2e047f
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-11 22:20:25 -0300

    create function to give convert sales chart information

--- src/Controller/RelatoriosController.ts
+++ src/Controller/RelatoriosController.ts
@@ -24,4 +24,35 @@ export class RelatoriosController {
     return res.json(pedidos);
   }
 
+  async conversaoDeVendas(req: Request, res: Response) {
+    const pedidos = await prisma.pedido.findMany({
+      where: {
+        data_pedido: {
+          gte: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000),
+        },
+        Pagamento: {
+          status: "Pago",
+        },
+      },
+      include: {
+        vendas: true,
+        alugueis: true,
+      },
+    });
+
+    const visitas = await prisma.visit.findMany({
+      where: {
+        date: {
+          gte: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000),
+        },
+      },
+    });
+
+
+
+    if(!pedidos || !visitas) return res.status(404).json({message: "Nenhum pedido encontrado"})
+    
+    return res.json({'pedido': pedidos, 'visitas': visitas});
+  }
+
 }
