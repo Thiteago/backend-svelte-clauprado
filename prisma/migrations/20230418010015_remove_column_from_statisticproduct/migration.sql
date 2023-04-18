9f72fa5982fd4e6ae1c4cb0c1cc942bf35c35b26
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-17 22:14:48 -0300

    removed qtdeEmPromocao column and added totalLucro to get better info to how much a product is selling

---
+++ prisma/migrations/20230418010015_remove_column_from_statisticproduct/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `qtdeEmPromocao` on the `StatisticProduct` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "StatisticProduct" DROP COLUMN "qtdeEmPromocao";
