ff148e211049890772a86b8e6cec297c9a59020a
Thiago <thiagodavidpd@gmail.com>
2023-04-20 23:38:35 -0300

    create route to delete sale

--- src/Routes/PromocaoRoute.ts
+++ src/Routes/PromocaoRoute.ts
@@ -9,6 +9,8 @@ routerPromocao.post("/promocao/cadastrar", promocaoController.cadastrar);
 
 routerPromocao.patch("/promocao/desabilitar/:id", promocaoController.desabilitar);
 
+routerPromocao.delete("/promocao/excluir/:id", promocaoController.excluir)
+
 routerPromocao.get("/promocao/listar", promocaoController.listar);
 routerPromocao.get("/promocao/promocoesativas", promocaoController.listarPromocaoAtiva)
 
