e76043cbc1fcb9ed78b822baecf05aa7bc5b7d71
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-02 21:20:40 -0300

    create two new columns on Aluguel table to store date of the devolution and tracking devolution code

---
+++ prisma/migrations/20230502233846_added_data_devolucao_to_aluguel_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Aluguel" ADD COLUMN     "data_devolucao" TIMESTAMP(3);
---
+++ prisma/migrations/20230502235316_added_new_column_to_store_code_of_the_devolution_tracking/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Aluguel" ADD COLUMN     "codigo_rastreio_devolucao" TEXT;
