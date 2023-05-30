31e641f5a749624eef7f538c4e52aa670789f79b
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-30 20:28:08 -0300

    create update and delete methods to change slide data

--- src/Controller/SlideController.ts
+++ src/Controller/SlideController.ts
@@ -4,7 +4,7 @@ import { prisma } from "../utils/prisma";
 export class SlideController{
   async cadastrar(req: Request, res: Response){
     let ids: Array<string> = [];
-    var myfiles = JSON.parse(JSON.stringify(req.files))
+    let myfiles = JSON.parse(JSON.stringify(req.files))
     
     myfiles.map((item: any) => {
       //remove any special characters from the filename but keep the extension
@@ -29,6 +29,41 @@ export class SlideController{
   }
 
   async alterar(req: Request, res: Response){
+    const { id } = req.params;
+    const { titulo, descricao, link } = req.body;
+    let myfiles = JSON.parse(JSON.stringify(req.files))
+    let ids: Array<string> = [];
+
+    if(myfiles.length > 0){
+      myfiles.map((item: any) => {
+        item.filename = item.filename.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^a-zA-Z0-9 ._-]/g, '');
+        ids.push(item.filename)
+      })
+      await prisma.slide.update({
+        where: {
+          id: Number(id)
+        },
+        data: {
+          titulo,
+          descricao,
+          link,
+          imagem: ids[0],
+        }
+      })
+    }else {
+      await prisma.slide.update({
+        where: {
+          id: Number(id)
+        },
+        data: {
+          titulo,
+          descricao,
+          link,
+        }
+      })
+    }
+
+    return res.status(200).json({message: "Slide alterado com sucesso!"})
   }
 
   async listar(req: Request, res: Response){
@@ -39,5 +74,16 @@ export class SlideController{
     })
 
     return res.status(200).json(slides)
+  }
+
+  async excluir(req: Request, res: Response){
+    const { id } = req.params;
+    const slide = await prisma.slide.delete({
+      where: {
+        id: Number(id)
+      }
+    })
+
+    return res.status(200).json(slide)
   }
 }
