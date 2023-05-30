f2a8f5cb15cdded94c99b92cef1ffe3a1a0f3651
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-29 22:13:02 -0300

    added replace to store filename without special data and method to list data slide

--- src/Controller/SlideController.ts
+++ src/Controller/SlideController.ts
@@ -7,24 +7,37 @@ export class SlideController{
     var myfiles = JSON.parse(JSON.stringify(req.files))
     
     myfiles.map((item: any) => {
+      //remove any special characters from the filename but keep the extension
+
+      item.filename = item.filename.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^a-zA-Z0-9 ._-]/g, '');
+      
       ids.push(item.filename)
     })
 
     const { titulo, descricao, link } = req.body;
-
     const slide = await prisma.slide.create({
       data: {
         titulo,
         descricao,
         link,
-        imagens: ids,
+        imagem: ids[0],
       }
     })
 
     return res.status(201).json(slide)
-    
+
   }
 
   async alterar(req: Request, res: Response){
+  }
+
+  async listar(req: Request, res: Response){
+    const slides = await prisma.slide.findMany({
+      orderBy: {
+        id: "desc"
+      }
+    })
+
+    return res.status(200).json(slides)
   }
 }
